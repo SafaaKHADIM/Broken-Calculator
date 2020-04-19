dd950b18f79af83d181cd792055fd32e25605e30
safaa <khadim.safaa@gmail.com>
2020-04-19 21:17:53 +0000

    general design of the calculator

--- src/App.js
+++ src/App.js
@@ -10,15 +10,56 @@ function App() {
         <main className="flexbox">
           <Board id="board-1" className="board">
             <Card id="card-1" className="card" draggable="true" idblock="board-1">
-              <button>card 1</button>
+              <button> 1</button>
             </Card>
           </Board>
           <Board id="board-2" className="board">
             <Card id="card-2" className="card" draggable="true" idblock="board-2">
-              <button>card 2</button>
+              <button> 2</button>
+            </Card>
+          </Board>
+          <Board id="board-3" className="board">
+            <Card id="card-3" className="card" draggable="true" idblock="board-3">
+                <button> 3</button>
             </Card>
           </Board>
         </main>
+          <br/>
+          <main className="flexbox">
+              <Board id="board-4" className="board">
+                  <Card id="card-4" className="card" draggable="true" idblock="board-4">
+                      <button> 4</button>
+                  </Card>
+              </Board>
+              <Board id="board-5" className="board">
+                  <Card id="card-5" className="card" draggable="true" idblock="board-5">
+                      <button> 5</button>
+                  </Card>
+              </Board>
+              <Board id="board-6" className="board">
+                  <Card id="card-6" className="card" draggable="true" idblock="board-6">
+                      <button> 6</button>
+                  </Card>
+              </Board>
+          </main>
+          <br/>
+          <main className="flexbox">
+              <Board id="board-7" className="board">
+                  <Card id="card-7" className="card" draggable="true" idblock="board-7">
+                      <button> 7</button>
+                  </Card>
+              </Board>
+              <Board id="board-8" className="board">
+                  <Card id="card-8" className="card" draggable="true" idblock="board-8">
+                      <button> 8</button>
+                  </Card>
+              </Board>
+              <Board id="board-9" className="board">
+                  <Card id="card-9" className="card" draggable="true" idblock="board-9">
+                      <button> 9</button>
+                  </Card>
+              </Board>
+          </main>
       </div>
   );
 }
